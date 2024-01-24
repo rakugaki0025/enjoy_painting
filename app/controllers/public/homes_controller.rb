98e76461df68dcc5bcd82631b5bd99d5fe9f3255
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2024-01-24 06:06:02 +0000

    deployment_Actual_performance_41

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,40 +3,28 @@ class Public::HomesController < ApplicationController
   
     ## 顧客_トップページ_root_path
   def top
-      
         ## イラストデータを入れる
       @illustration = Illustration.new
-      
         ## 全イラストデータ取得
       @illustrations = Illustration.all
-      
         ## イラストに紐づく会員情報取得
       @illustrations = Illustration.includes(:customer).all
-      
         ## "genre_id"が存在_"params:genre_id"と一致,取得__present?が大事
       @illustrations = @illustrations.where(genre_id: params[:genre_id]) if params[:genre_id].present?
-      
         ## さらに"name"が存在する場合は,名前"name"に"params:name"を部分一致させるデータのみ取得
       @illustrations = @illustrations.where('name LIKE(?)', "%#{params[:name]}%") if params[:name].present?
-      
         ## 作成日時の降順に並び替え,6件ずつ表示
       @illustrations = @illustrations.order(created_at: :desc).page(params[:page]).per(6)
-      
         ## サンプルデータを入れる
       @sample_illustration = SampleIllustration.new
-      
         ## 全サンプルデータ取得
       @sample_illustrations = SampleIllustration.all
-      
         ## サンプルに紐づくレビューモデル情報取得
       @sample_illustrations = @sample_illustrations.includes(:reviews)
-      
         ## "genre_id"が存在_"params:genre_id"と一致,取得__present?が大事
       @sample_illustrations = @sample_illustrations.where(genre_id: params[:genre_id]) if params[:genre_id].present?
-      
         ## さらに"name"が存在する場合は,名前"name"に"params:name"を部分一致させるデータのみ取得
       @sample_illustrations = @sample_illustrations.where('name LIKE(?)', "%#{params[:name]}%") if params[:name].present?
-      
         ## 作成日時の降順に並び替え,6件ずつ表示
       @sample_illustrations = @sample_illustrations.order(created_at: :desc).page(params[:page]).per(6)
         
