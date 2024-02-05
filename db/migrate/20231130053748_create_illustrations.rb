10f21c569000b994bbbef81cdb1a94ed163e76c5
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2024-02-05 13:30:18 +0000

    deployment_Actual_performance_54

--- db/migrate/20231130053748_create_illustrations.rb
+++ db/migrate/20231130053748_create_illustrations.rb
@@ -4,13 +4,11 @@ class CreateIllustrations < ActiveRecord::Migration[6.1]
   
   def change
     create_table :illustrations do |t|
-      
         ## 追加
       t.integer :customer_id, null: false          # t.データ型 :カラム名, null: false
       t.integer :genre_id, null: false             # t.データ型 :カラム名, null: false
       t.string :name, null: false                  # t.データ型 :カラム名, null: false
       t.text :introduction, null: false            # t.データ型 :カラム名, null: false
-      
         ## 初回生成される created_at, updated_at
       t.timestamps
     end
